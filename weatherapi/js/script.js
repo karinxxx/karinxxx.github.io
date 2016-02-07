795466a7dda69ba6705f0cca874b47ffca92508c
karinxxx <karin1@iwanamikarin-no-MacBook-Pro.local>
2016-02-07 11:43:51 +0900

    up

--- weatherapi/js/script.js
+++ weatherapi/js/script.js
@@ -5,5 +5,21 @@ $(function(){
         var lon = pos.coords.longitude;
 
         console.log(lat,lon);
+
+        //定数　変わらない
+        const apikey = '6416cd90d913c3b8ed55195b319d5ac7';
+        //openweathermapのお天気情報のURL
+        var url = 'api.openweathermap.org/data/2.5/weather?lat='+ lat + '&lon=' + lon + '&appid=' + apikey;
+
+        $.ajax({
+            url:url,
+            dateType:'json'
+        }).done(function(date){
+
+            console.log(date);
+
+        }).fail(function(){
+            console.log('ajax error!!!');
+        });
     });
 });
